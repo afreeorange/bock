63df28b9472ff89b5bb8dfb4b157a04f7862cc07
Nikhil Anand <mail@nikhil.io>
2016-04-09 12:40:22 -0500

    Change copyright year

--- bock/__init__.py
+++ bock/__init__.py
@@ -1,9 +1,12 @@
+from datetime import datetime
+
 from .factory import create_wiki
+
 
 __title__ = 'wiki_nikhil_io'
 __version__ = '1.0.23'
 __author__ = 'Nikhil Anand'
 __license__ = 'MIT'
-__copyright__ = '(c) 2016 Nikhil Anand'
+__copyright__ = '(c) {} Nikhil Anand'.format(datetime.now().year)
 
 # instance = create_wiki(debug=True)
