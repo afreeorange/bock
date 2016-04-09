529806bb976a97c243338ce194bbb6fc60a52b08
Nikhil Anand <mail@nikhil.io>
2016-04-09 12:12:49 -0500

    Bump version: 1.0.21 → 1.0.22

--- bock/__init__.py
+++ bock/__init__.py
@@ -1,7 +1,7 @@
 from .factory import create_wiki
 
 __title__ = 'wiki_nikhil_io'
-__version__ = '1.0.21'
+__version__ = '1.0.22'
 __author__ = 'Nikhil Anand'
 __license__ = 'MIT'
 __copyright__ = '(c) 2016 Nikhil Anand'
