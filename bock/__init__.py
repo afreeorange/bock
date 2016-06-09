6dc58262a59721adc1e8ac319b39cbd524ab90e3
Nikhil Anand <mail@nikhil.io>
2016-06-08 21:34:36 -0500

    Bump version: 1.2.9 → 1.2.10

--- bock/__init__.py
+++ bock/__init__.py
@@ -9,7 +9,7 @@ instance = create_wiki(debug=True, search=False, refresh_index=False)
 
 # Package metadata
 __title__ = 'bock'
-__version__ = '1.2.9'
+__version__ = '1.2.10'
 __author__ = 'Nikhil Anand'
 __license__ = 'MIT'
 __copyright__ = '(c) {} Nikhil Anand'.format(datetime.now().year)
