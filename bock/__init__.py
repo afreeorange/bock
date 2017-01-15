6aae071ca85891e451019841778ba26d97ff5b8d
Nikhil Anand <mail@nikhil.io>
2017-01-14 23:24:24 -0600

    Bump version: 1.7.5 → 1.8.0

--- bock/__init__.py
+++ bock/__init__.py
@@ -12,7 +12,7 @@ if os.getenv('DEBUG'):
 
 # Package metadata
 __title__ = 'bock'
-__version__ = '1.7.5'
+__version__ = '1.8.0'
 __author__ = 'Nikhil Anand'
 __license__ = 'MIT'
 __copyright__ = '(c) {} Nikhil Anand'.format(datetime.now().year)
