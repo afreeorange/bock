7308632f3715157177c6527c0a5427b24fe4432a
afreeorange <anand.nikhil@gmail.com>
2026-08-03 13:34:23 -0500

    Final edits

--- theme/components/Base.tsx
+++ theme/components/Base.tsx
@@ -16,20 +16,7 @@ export function Base(props: any) {
         <link rel="icon" href="/img/favicon.png" />
         <link rel="apple-touch-icon" href="/img/logo192.png" />
         <link rel="stylesheet" href="/css/styles.css" />
-        <link rel="stylesheet" href="/css/highlight.css" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Alegreya:ital,wght@0,400..900;1,400..900&display=swap"
-          rel="stylesheet"
-        />
         <title>{title} – Nikhil's Personal Wiki</title>
-        <script
-          type="text/javascript"
-          id="MathJax-script"
-          defer
-          src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js"
-        ></script>
         <script
           defer
           data-domain="wiki.nikhil.io"
@@ -55,17 +42,6 @@ export function Base(props: any) {
           "keypress", (e) => e.key === "f"
           ? window.location.assign("/archive")
           : null);
-        window.MathJax = {
-          tex: {
-            inlineMath: [
-              ['$', '$'],
-              ['\\\\(', '\\\\)']
-            ]
-          },
-          svg: {
-            fontCache: 'global'
-          }
-        };
       `,
             }}
           />
--- theme/components/Hierarchy.tsx
+++ theme/components/Hierarchy.tsx
@@ -4,49 +4,75 @@ export function Hierarchy(props: any) {
   if (!nodes || nodes.length === 0) return null;
 
   return (
-    <article-breadcrumbs>
+    <article-hierarchy>
       <ul>
-        <li>
-          <a data-entity-type="folder" href="/ROOT" title="ROOT">
-            Root
-          </a>
-        </li>
-        <li>
-          <a data-entity-type="folder" href="/Food" title="Food">
-            Food
-          </a>
-        </li>
-        <li>
-          <a
-            data-entity-type="folder"
-            href="/Food/Thai_Curry_Experiments"
-            title="Thai Curry Experiments"
-          >
-            Thai Curry Experiments
-          </a>
-        </li>
-        <li>
-          <a
-            data-entity-type="article"
-            href="/Food/Thai_Curry_Experiments/Thai_Green_Curry_Chicken_-_Instant_Pot"
-            title="Thai Green Curry Chicken - Instant Pot"
-          >
-            Thai Green Curry Chicken - Instant Pot
-          </a>
-        </li>
-        <li>
-          <a
-            data-entity-type="revision-list"
-            href="/Food/Thai_Curry_Experiments/Thai_Green_Curry_Chicken_-_Instant_Pot/revisions"
-            title="Article revisions"
-          >
-            Revisions
-          </a>
-        </li>
-        <li>
-          <span class="single-revision-title">Revision 24364775</span>
-        </li>
+        {nodes.map((node: HierarchicalEntity) => (
+          <li>
+            <a
+              data-entity-type={node.Type}
+              href={node.Name === "ROOT" ? "/ROOT" : "/" + node.URI}
+              title={node.Name}
+            >
+              {node.Name === "ROOT" ? "Root" : node.Name}
+            </a>
+          </li>
+        ))}
+
+        {type === "raw" && (
+          <li>
+            <span>Raw</span>
+          </li>
+        )}
+
+        {type === "revision" && (
+          <>
+            <li>
+              <a
+                data-entity-type="revision-list"
+                href={uri + "/revisions"}
+                title="Article revisions"
+              >
+                Revisions
+              </a>
+            </li>
+            <li>
+              <span className={"single-revision-title"}>{"Revision " + (revision && revision.ShortId)}</span>
+            </li>
+          </>
+        )}
+
+        {type === "revision-raw" && (
+          <>
+            <li>
+              <a
+                data-entity-type="revision-list"
+                href={uri + "/revisions"}
+                title="Article revisions"
+              >
+                Revisions
+              </a>
+            </li>
+            <li>
+              <a
+                data-entity-type="revision"
+                href={uri + "/revisions/" + (revision && revision.ShortId)}
+                title={"View revision " + (revision && revision.ShortId)}
+              >
+                {"Revision " + (revision && revision.ShortId)}
+              </a>
+            </li>
+            <li>
+              <span>Raw</span>
+            </li>
+          </>
+        )}
+
+        {type === "revision-list" && (
+          <li>
+            <span className={"multiple-revisions-title"}>Revisions</span>
+          </li>
+        )}
       </ul>
-    </article-breadcrumbs>
+    </article-hierarchy>
   );
 }
